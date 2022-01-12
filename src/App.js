2abb5f51bf85245940afd53e34e1e1ab2569e5f5
imcarissa <carissa.sarreal@gmail.com>
2022-01-12 01:23:10 -0500

    About component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './Header'
 import Contact from './Contact'
 import Footer from './Footer'
+import About from './About'
 
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <About />
       <Contact />
       <Footer />
     </div>
